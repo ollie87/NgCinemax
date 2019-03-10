7e13fe250ad6472ced0597159ff8f8be8b3fdb5b
Mario <mariogv87@gmail.com>
2019-03-10 20:08:14 +0100

    navbar-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarSocialComponent } from './landing-page/navbar-social/navbar-social.component';
+import { NavbarMenuComponent } from './landing-page/navbar-menu/navbar-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    NavbarSocialComponent
+    NavbarSocialComponent,
+    NavbarMenuComponent
   ],
   imports: [
     BrowserModule
