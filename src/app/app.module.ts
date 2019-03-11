d6e27a4df9284f258614ad50be65526b1b779e98
Mario <mariogv87@gmail.com>
2019-03-11 20:39:12 +0100

    add filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarSocialComponent } from './landing-page/navbar-social/navbar-social.component';
 import { NavbarMenuComponent } from './landing-page/navbar-menu/navbar-menu.component';
+import { MoviesFilterComponent } from './landing-page/movies-filter/movies-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     NavbarSocialComponent,
-    NavbarMenuComponent
+    NavbarMenuComponent,
+    MoviesFilterComponent
   ],
   imports: [
     BrowserModule
