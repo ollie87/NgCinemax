3de149d8ad5726ad4cf54bbd0b286ade98ca6daa
Mario <mariogv87@gmail.com>
2019-03-16 19:28:16 +0100

    section carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MoviesFilterComponent } from './landing-page/movies-filter/movies-filte
 import { SlidesComponent } from './landing-page/slides/slides.component';
 import { WeeklyBilboardsComponent } from './landing-page/weekly-bilboards/weekly-bilboards.component';
 import { PremieresComponent } from './landing-page/premieres/premieres.component';
+import { CarouselComponent } from './landing-page/carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { PremieresComponent } from './landing-page/premieres/premieres.component
     MoviesFilterComponent,
     SlidesComponent,
     WeeklyBilboardsComponent,
-    PremieresComponent
+    PremieresComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
