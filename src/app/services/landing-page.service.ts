4f6c470f6e99b73fa28e19feb01a052385740a23
Mario <mariogv87@gmail.com>
2019-03-24 13:05:18 +0100

    movie service

--- src/app/services/landing-page.service.ts
+++ src/app/services/landing-page.service.ts
@@ -3,15 +3,20 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.prod';
 import { SocialNetwork } from '../landing-page/navbar-social/social-network';
+import { Movie } from '../models/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LandingPageService {
-  apiRedes:string = 'RedesSociales.json';
+  apiRedes = 'RedesSociales.json';
+  apiFilms = 'Peliculas.json';
   constructor(public _http: HttpClient) { }
 
   getSocialNetworks(): Observable<SocialNetwork[]> {
     return this._http.get<SocialNetwork[]>(`${environment.API}${this.apiRedes}`);
+  }
+  getMovies(): Observable<Movie[]> {
+    return this._http.get<Movie[]>(`${environment.API}${this.apiFilms}`);
   }
 }
