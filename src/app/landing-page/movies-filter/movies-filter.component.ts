8dc7bd872c6375cae6b3b817d807ea910876a2f7
Mario <mariogv87@gmail.com>
2019-03-26 20:11:33 +0100

    add select films

--- src/app/landing-page/movies-filter/movies-filter.component.ts
+++ src/app/landing-page/movies-filter/movies-filter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { create } from 'domain';
+import { Movie } from 'src/app/models/movie';
+import { LandingPageService } from 'src/app/services/landing-page.service';
 declare var $: any;
 @Component({
   selector: 'app-movies-filter',
@@ -9,14 +10,16 @@ declare var $: any;
 export class MoviesFilterComponent implements OnInit {
   fechas: Date[] = [];
   formFilter = {
-    date: new Date()
+    date: new Date(),
   };
+  films: Movie[] = [];
 
-  constructor() { }
+  constructor( public _landingService: LandingPageService ) { }
 
   ngOnInit() {
     $('select').formSelect();
     this.createDates();
+    this.getBilboards();
   }
 
   createDates() {
@@ -25,6 +28,12 @@ export class MoviesFilterComponent implements OnInit {
       date.setDate(date.getDate() + i);
       this.fechas.push(date);
     }
+  }
+
+  getBilboards() {
+    this._landingService.getMovies().subscribe((films) => {
+      this.films = films.filter(film => film.cartelera);
+    });
   }
 
 }
